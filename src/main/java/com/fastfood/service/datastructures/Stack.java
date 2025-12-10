c3c18d86e56ca65ff6943ab5aab99d06bc9b5c20
SylvannaCiss <20243ds146@utez.edu.mx>
2025-12-09 19:15:25 -0600

    ARREGLE EL STACK

--- src/main/java/com/fastfood/service/datastructures/Stack.java
+++ src/main/java/com/fastfood/service/datastructures/Stack.java
@@ -1,43 +1,81 @@
 package com.fastfood.service.datastructures;
 
+import java.util.Arrays;
+
 import com.fastfood.service.model.HistorialOperacion;
 
 
 public class Stack {
-    
+
     private HistorialOperacion[] data; 
     private int top;
 
-    public Stack(int capacity) {
-        data = new HistorialOperacion[capacity];
-        top = -1;
+   public Stack() {
+        this(10);
+    }
+
+    public Stack(int initialCpacity) {
+        this.data = new HistorialOperacion[initialCpacity];
+    }
+
+    public void push(HistorialOperacion value) {
+        addCapacity();
+        this.data[top++] = value;
+    }
+
+    public HistorialOperacion pop() {
+        if(isEmpty()){
+            System.out.println("No hay elementos en el Stack");
+            return null;
+        }
+        HistorialOperacion value = (HistorialOperacion) data[--top];
+        data[top] = null;
+        return value;
+    }
+
+    public HistorialOperacion peek() {
+        if(isEmpty()){
+            System.out.println("No hay elementos en el Stack");
+            return null;
+        }
+        return (HistorialOperacion) data[top - 1];
     }
 
     public boolean isEmpty() {
         return top == 0;
     }
 
-    pub
-
-
-
-    
-    public void push(HistorialOperacion op) {
-        Node newNode = new Node(op);
-        newNode.next = top;
-        top = newNode;
+    public int size() {
+        return top;
     }
 
-    // Operación: Pop (Sacar del tope)
-    public HistorialOperacion pop() {
-        if (isEmpty()) {
-            return null;
+    public void clear() {
+        if(isEmpty()){
+            System.out.println("No hay elementos en el Stack");
         }
-        HistorialOperacion op = top.data;
-        top = top.next;
-        return op;
+        for (int i = top-1; i >= 0; i--) {
+            data[i] = null;
+        }
     }
 
-    
+    public void addCapacity(){
+        if(top == data.length) { //Si el arreglo está lleno, vamos a aumentar la capacidad
+            int newCap = data.length*2;
+            //Vamos a crear un nuevo arreglo y pasarle los datos que están actualmente al nuevo
+            data = Arrays.copyOf(data, newCap);
+        }
+    }
+
+    public void print() {
+        if(isEmpty()){
+            System.out.println("No hay elementos en el Stack");
+        }
+        for (int i = top-1; i >= 0; i--) {
+            if(data[i] != null){
+                System.out.println(data[i]);
+            }
+        }
+    }
+
    
 }
